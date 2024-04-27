c411648cb046c11acfc40d74e521262c5bd6e241
Ash Cozey <62861333+Ash-Cozey@users.noreply.github.com>
2024-04-26 18:37:32 -0600

    Update keymap.c

--- keyboards/thumbsup/rev7_rp2040_split_5x12/keymaps/default_5x12_v7_via/keymap.c
+++ keyboards/thumbsup/rev7_rp2040_split_5x12/keymaps/default_5x12_v7_via/keymap.c
@@ -58,10 +58,10 @@ Single hit:   |		       |      |Space ||Space |      |                 |
  */
 [_BASE] = LAYOUT_ortho_5x12( \
   KC_GRV,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,       KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINUS,  \
-  KC_ESC,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_DELETE, \
+  KC_ESC,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_DEL, \
   KC_TAB,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,       KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOTE,  \
-  KC_BSPACE,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_N,   KC_M,   KC_COMMA,KC_DOT, KC_SLSH,KC_MINUS, \
-    LGUI_T(KC_CAPSLOCK),KC_LALT,LSFT_T(KC_BSPACE),LCTL_T(KC_TAB),LT3(KC_SPACE),  LT3(KC_SPACE),RCTL_T(KC_DELETE),KC_SFTENT, KC_RALT,RGUI_T(KC_BSLASH)   \
+  KC_BSPC,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_N,   KC_M,   KC_COMMA,KC_DOT, KC_SLSH,KC_MINUS, \
+    LGUI_T(KC_CAPS),KC_LALT,LSFT_T(KC_BSPACE),LCTL_T(KC_TAB),LT(3,KC_SPACE),  LT(2,KC_SPACE),RCTL_T(KC_DELETE),KC_SFTENT, KC_RALT,RGUI_T(KC_BSLASH)   \
 ),
 
 
@@ -157,7 +157,7 @@ without separate thumbcluster.
 [_FUNC] = LAYOUT_ortho_5x12( \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
   _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  LT(_EXTRARIGHT,KC_TAB), KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9, KC_F10,  _______, \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9, KC_F10,  _______, \
   _______, KC_F11,  KC_F12,  KC_LALT, KC_LSFT, KC_LALT, KC_RALT, KC_RSFT, KC_COMM, KC_DOT,  KC_SLSH, _______, \
     	    		     _______,      _______,      _______,      _______, _______, _______,  _______, _______, _______, _______           \
 ),
@@ -181,7 +181,7 @@ void matrix_scan_user(void) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, _CRSRGHT, _CRSLEFT, _NUMBERS);
+    return update_tri_layer_state(state, _NAV, _NUM, _FUNC);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {

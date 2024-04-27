a28c06574c29af96f60da08a1c90416caee28010
Ash Cozey <62861333+Ash-Cozey@users.noreply.github.com>
2024-04-26 18:51:56 -0600

    Update keymap.c

--- keyboards/thumbsup/rev7_rp2040_split_5x12/keymaps/default_5x12_v7_via/keymap.c
+++ keyboards/thumbsup/rev7_rp2040_split_5x12/keymaps/default_5x12_v7_via/keymap.c
@@ -61,7 +61,7 @@ Single hit:   |		       |      |Space ||Space |      |                 |
   KC_ESC,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_DEL, \
   KC_TAB,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,       KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOTE,  \
   KC_BSPC,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_N,   KC_M,   KC_COMMA,KC_DOT, KC_SLSH,KC_MINUS, \
-    LGUI_T(KC_CAPS),KC_LALT,LSFT_T(KC_BSPACE),LCTL_T(KC_TAB),LT(3,KC_SPACE),  LT(2,KC_SPACE),RCTL_T(KC_DELETE),KC_SFTENT, KC_RALT,RGUI_T(KC_BSLASH)   \
+    LGUI_T(KC_CAPS),KC_LALT,LSFT_T(KC_BSPC),LCTL_T(KC_TAB),LT(3,KC_SPACE),  LT(2,KC_SPACE),RCTL_T(KC_DEL),SC_SENT, KC_RALT,RGUI_T(KC_BSLS)   \
 ),
 
 
